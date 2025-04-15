e24523a534a17a82ee2f519e062099cec878a988
Isaac Mckinney (RIT Student) <ism1762@rit.edu>
2025-04-15 18:04:39 -0400

    commit update

--- swen-project-react-app-API-tests/stubbed/testPostMultipleData.py
+++ swen-project-react-app-API-tests/stubbed/testPostMultipleData.py
@@ -2,18 +2,21 @@ import requests
 import pytest
 
 # Base URL of the API
-BASE_URL = "https://3tpxypd416.execute-api.us-east-1.amazonaws.com/trailplanner_api_stage/trail_data/"
+BASE_URL = "https://h12yxdhdlj.execute-api.us-east-1.amazonaws.com/trailplanner_api_stage/trail_data/"
 
 headers = {
-    "Authorization": "eyJraWQiOiJwMUpyNWQycUx0T0x4WUVINmZ6RXpOSm1LcSttK0xCRmZEOFdvMVNPanM4PSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJiNGE4MTRmOC0yMDAxLTcwMDgtNjVmMC04NjZiZTk3YjNjMTgiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfWDl0V0FRM3F3IiwiY29nbml0bzp1c2VybmFtZSI6ImI0YTgxNGY4LTIwMDEtNzAwOC02NWYwLTg2NmJlOTdiM2MxOCIsIm9yaWdpbl9qdGkiOiIxNDRhZjkxMi04MjFmLTRjNjAtOTE2Mi0wMGEwZmFlN2UxZDEiLCJhdWQiOiIxYThqZDE3M3RpdjhsNTlkNTM4M2tmMDVibSIsImV2ZW50X2lkIjoiYzM5OTQ3YjgtMTA3My00YzYyLWI1ZWQtMzY4ZDg0M2JhNTgzIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE3NDM3MDg0NDcsImV4cCI6MTc0MzcxMjA0NywiaWF0IjoxNzQzNzA4NDQ3LCJqdGkiOiI0NTk1OWI3MS04MTRkLTQzN2UtODA2ZC02ZDNlMzY0OTliYTMiLCJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSJ9.UTJoM7zc2mXIzK8j8hsgLipvzPCdgC_qVsWWky3HpCWHv1ddFGPyAJR_CDU637UoiCDVY04COMZRUXwAlOnW3D1NNB90bHpQd8Nmmllj6D8F4ZxoFOZg3Q_JjLoQG61NqpMkDz7OY9R7-SnI81Ni5fJX-yzT5fNlhv3_7elPNMgthMLi_Cx5-16wxpiteJvfHkxRX_GhweTKlA15tZMoRGjpf1kc0cpQAERq8SG9QDsD_SIb5D39bngEnbscvMwDtBCsUGE09USnbTyNNibMLWiT96iSEvQSeMIVINf0dzWbD6laIKHH5AYXWYpaTEIgx5OWlS3FmiYiJ6OGIvdKYg",
+    "Authorization": "eyJraWQiOiJKWlcxNG1oWnR6TkF2NTFXWnFVT3pUY1ZqT2dZZVF2SU5vT21FV2pmOWFjPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIyNDE4MjRjOC1lMDIxLTcwOTQtZDU4ZC1mMzllZGQ5N2NiZjYiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfSHVNbzgwa0JMIiwiY29nbml0bzp1c2VybmFtZSI6IjI0MTgyNGM4LWUwMjEtNzA5NC1kNThkLWYzOWVkZDk3Y2JmNiIsIm9yaWdpbl9qdGkiOiJjZmJkMjY2NC0zMGNmLTQxYTItOTE0NS05YjA0ODA4MjA5ODIiLCJhdWQiOiI0bmQxcTMwZzR2OGF2cTdrbzA4MDNjbG4wOSIsImV2ZW50X2lkIjoiYzQwMTdlN2YtYmNjMC00MzlhLTkzOTItMzdkMzU2N2JjOGE2IiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE3NDQ3NTM4NzMsImV4cCI6MTc0NDc1NzQ3MywiaWF0IjoxNzQ0NzUzODczLCJqdGkiOiIzNGI4NmUzMi0wNGE4LTQzMzctYjUyMS1hYjZhOTk0NmVmNjAiLCJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSJ9.mIXBvJUYsImcE5jgEH2huDK1Gu_mCALw7zlJ9a-oV7vyvjx2GmI7wvTDBjKqtxnC1IbiLNtuI8YAkW7-pydDloymFttMPrO-d4At8orkl1ouNMnCmT8e9wc5KSLLTGts_CzgLYw5PTEmkTaSDrw4rDMqRqlKRmxIt7G1T0F1F-h-tOSi52h2W9mvVpFwF-LgrgM_DJVEDoH1k_-Fa-qSNLmKhTubUoiHfo-DaGUqOz6Z3Pc_89z5bQm3Rc3edLZqoNMspdqKWP1TIvJvjVnML10DswyGd_dycXWc2QgS_1P_kz6tyLHcU7jSzqEYre7fEFMFGQ_kdtCHu51SNMS-hA",
     "Content-Type": "application/json"
 }
 
-@pytest.mark.API_testbed
+@pytest.mark.Testbed
 def test_post_request():
     payload = {
-        "id": 7,
-        "timestamp": 1735691235
+        "id" : 1,
+        "data" : [
+        {"timestamp" : 37334782784}, {"timestamp": 37334782764}, {"timestamp": 47777777777}
+        ]
+
     }
 
     response = requests.post(BASE_URL, json=payload, headers=headers)
