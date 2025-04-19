63c774a0fc06575345c5f4c2223f382b086da7c9
pwc9501 <pwc9501@g.rit.edu>
2025-04-18 22:24:47 -0400

    added aws setup instructions

--- setup/macos-setup.sh
+++ setup/macos-setup.sh
@@ -3,9 +3,39 @@
 # basic setup
 touch ~/.zshrc
 xcode-select --install
+source ~/.zshrc
 
-nvm_install_status=$(which nvm)
-if [[ $nvm_install_status =~ ".*not found" ]]; then
+aws_setup_instructions() {
+    echo "The following instructions will walk through the process of setting up an AWS account and linking it to the AWS CLI"
+    vared -p "Press Enter to Continue" -c tmp
+
+    echo "Navigate to http://aws.amazon.com, sign in to an existing account, or create a new account."
+    vared -p "Press Enter to Continue" -c tmp
+
+    echo "Once logged in, navigate to the top right and click your username. Select 'Security credentials' from the dropdown menu."
+    vared -p "Press Enter to Continue" -c tmp
+
+    echo "Scroll down to the section labeled 'Access keys' and follow the prompts to create an access key."
+    echo "There will be warning against creating an access key for the root account, it is okay to ignore these warnings."
+    echo "Make sure to keep the page that displays the new access key open for the following steps."
+    vared -p "Press Enter to Continue" -c tmp
+
+    echo "The AWS CLI will now be invoked to link your account to the CLI."
+    echo "When prompted, enter your access key and press Enter."
+    echo "Do the same for your secret access key."
+    echo "'Default region name' and 'Default output format' can be left blank"
+    vared -p "Press Enter to Continue" -c tmp
+
+    aws configure
+
+    aws_test=$(aws sts get-caller-identity)
+    if [ $? -ne 0 ] ; then
+        echo "AWS Account linking failed, exiting."
+    fi
+}
+
+nvm_install_status=$(nvm --version)
+if [[ $? -ne 0 ]]; then
     echo "nvm not found, installing"
     # install nvm
     # source: https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating
@@ -14,7 +44,7 @@ if [[ $nvm_install_status =~ ".*not found" ]]; then
 fi
 
 node_install_status=$(which node)
-if [[ $node_install_status =~ ".*not found" ]]; then
+if [[ $? -ne 0 ]]; then
     # install latest node lts
     nvm install --lts
     source ~/.zshrc
@@ -22,7 +52,7 @@ fi
 
 
 aws_cli_install_status=$(which aws)
-if [[ $aws_cli_install_status =~ ".*not found" ]]; then
+if [[ $? -ne 0 ]]; then
     echo "AWS CLI not found, installing"
     # install the aws cli
     # source: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
@@ -34,7 +64,7 @@ fi
 
 
 homebrew_install_status=$(which brew)
-if [[ $homebrew_install_status =~ ".*not found" ]]; then
+if [[ $? -ne 0 ]]; then
     echo "homebrew not found, installing"
     # install homebrew
     # source: https://github.com/Homebrew/install
@@ -43,15 +73,17 @@ if [[ $homebrew_install_status =~ ".*not found" ]]; then
 fi
 
 terraform_install_status=$(which terraform)
-if [[ $homebrew_install_status =~ ".*not found" ]]; then
+if [[ $? -ne 0 ]]; then
     echo "terraform not found, installing"
     # install terraform 
     # source: https://developer.hashicorp.com/terraform/install
     brew tap hashicorp/tap
     brew install hashicorp/tap/terraform
+    source ~/.zshrc
 fi
 
-source ~/.zshrc
+aws_setup_instructions
 
+echo "Setup complete, exiting.
 
 
