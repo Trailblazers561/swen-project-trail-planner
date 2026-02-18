848e840c89fbc570bcf1fabfa36d0539dfc09dde
Richard Tang <rt6412@rit.edu>
2026-02-18 01:32:10 -0500

    component library and tailwindcss added

--- swen-project-react-app/vite.config.js
+++ swen-project-react-app/vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+ 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
+
+//previous vite config without tailwind
+// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
