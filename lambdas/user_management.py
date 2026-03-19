b3487c6389144c497066d3ae5dbad05b26e2f13f
EzDubzEz <tlwtee3@gmail.com>
2026-03-19 17:06:42 -0400

    updated authenticataion changes

--- lambdas/user_management.py
+++ lambdas/user_management.py
@@ -18,20 +18,19 @@ group_names_reversed = {USER: "user", TRAIL_MANAGER: "trail_manager", ADMIN: "ad
 
 # GET /user command
 def get_users(event, context):
+    print(event)
+
     try:
         params = event.get('queryStringParameters', {}) or {}
 
         target_user_role = params.get("target_user_role")
         if target_user_role and not user_groups.get(target_user_role):
-            return {
-                "statusCode": 400,
-                "headers": cors_headers(),
-                "body": json.dumps({"error": "Invalid target_user_role format"})
-            }
+            raise ValueError("Invalid target_user_role format")
         target_user_role = None
 
         max_count = int(params.get("max_count", 99))
 
+        print(f"Attempting to retrieve user information for role [{target_user_role}] up to [{max_count}] results")
         if not target_user_role:
             paginator = cognito.get_paginator('list_users')
             user_iterator = paginator.paginate(UserPoolId=COGNITO_USER_POOL_ID)
@@ -57,10 +56,18 @@ def get_users(event, context):
             user_formatted["role"] = max([group_names_reversed[group["GroupName"]] for group in groups["Groups"]], key=lambda x: user_groups[x], default="guest")
             users_formatted.append(user_formatted)
 
+        print(f"User information successfully retrieved: {user_formatted[:3]}")
         return {
             "statusCode": 200,
             "headers": cors_headers(),
             "body": json.dumps(users_formatted)
+        }
+    except ValueError as e:
+        print(e)
+        return {
+            "statusCode": 400,
+            "headers": cors_headers(),
+            "body": json.dumps({"error": f"{str(e)}"})
         }
     except Exception as e:
         print(e)
@@ -72,32 +79,24 @@ def get_users(event, context):
 
 # POST /user command
 def change_user_group(event, context):
+    print(event)
+
     try:
         body = json.loads(event.get("body") or "{}")
 
         target_user_id = body.get("target_user_id")
         target_user_role = body.get("target_user_role")
         if not target_user_id:
-            return {
-                    "statusCode": 400,
-                    "headers": cors_headers(),
-                    "body": json.dumps({"error": "Missing required field: target_username"})
-                }
+            raise ValueError("Missing required field: target_username")
         if not target_user_role:
-            return {
-                    "statusCode": 400,
-                    "headers": cors_headers(),
-                    "body": json.dumps({"error": "Missing required field: target_user_role"})
-                }
+            raise ValueError("Missing required field: target_user_role")
+
         if not user_groups.get(target_user_role):
-            return {
-                    "statusCode": 400,
-                    "headers": cors_headers(),
-                    "body": json.dumps({"error": "Invalid format for: target_user_role"})
-                }
+            raise ValueError("Invalid format for: target_user_role")
 
         caller_role = event.get("requestContext", {}).get("authorizer", {}).get("caller_role")
         if not caller_role:
+            print("Error: caller role not found when needed")
             return {
                     "statusCode": 400,
                     "headers": cors_headers(),
@@ -105,21 +104,15 @@ def change_user_group(event, context):
                 }
 
         if user_groups[target_user_role] >= user_groups[caller_role]:
-            return {
-                    "statusCode": 403,
-                    "headers": cors_headers(),
-                    "body": json.dumps({"error": "Invalid permissions to set user to specified target_user_role"})
-                }
+            raise PermissionError("Invalid permissions to set user to specified target_user_role")
+
+        print(f"Attempting to update user groups for user [{target_user_id}] to role [{target_user_role}] as a [{caller_role}]")
 
         end_target_groups = [group_names[group] for group in user_groups.keys() if user_groups[group] <= user_groups[target_user_role]]
         current_target_users_groups = cognito.admin_list_groups_for_user(Username=target_user_id, UserPoolId=COGNITO_USER_POOL_ID)["Groups"]
         current_target_user_role = max([group_names_reversed[group["GroupName"]] for group in current_target_users_groups], key=lambda x: user_groups[x], default="guest")
         if user_groups[current_target_user_role] >= user_groups[caller_role]:
-            return {
-                    "statusCode": 403,
-                    "headers": cors_headers(),
-                    "body": json.dumps({"error": "Invalid permissions to set user to specified target_user_role"})
-                }
+            raise PermissionError("Invalid permissions to set user to specified target_user_role")
         for group in current_target_users_groups:
             if group["GroupName"] in end_target_groups:
                 end_target_groups.remove(group["GroupName"])
@@ -128,10 +121,25 @@ def change_user_group(event, context):
         for group in end_target_groups:
             cognito.admin_add_user_to_group(UserPoolId=COGNITO_USER_POOL_ID, Username=target_user_id, GroupName=group)
 
+        print("Success: User groups updated")
         return {
             "statusCode": 200,
             "headers": cors_headers(),
             "body": json.dumps({"message": "User groups updated"})
+        }
+    except PermissionError as e:
+        print(e)
+        return {
+            "statusCode": 403,
+            "headers": cors_headers(),
+            "body": json.dumps({"error": f"{str(e)}"})
+        }
+    except ValueError as e:
+        print(e)
+        return {
+            "statusCode": 400,
+            "headers": cors_headers(),
+            "body": json.dumps({"error": f"{str(e)}"})
         }
     except Exception as e:
         print(e)
