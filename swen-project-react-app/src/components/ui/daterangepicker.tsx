28b284ab111a333b66b608e1ca1b760bab174f9d
Richard Tang <rt6412@rit.edu>
2026-03-30 15:22:04 -0400

    Overhauled date range picker, added granularity option and linked the two of them together

--- swen-project-react-app/src/components/ui/daterangepicker.tsx
+++ swen-project-react-app/src/components/ui/daterangepicker.tsx
@@ -1,6 +1,8 @@
 "use client"
-
 import * as React from "react"
+import { addDays, format } from "date-fns"
+import { CalendarIcon } from "lucide-react"
+import { type DateRange } from "react-day-picker"
 import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
 import { Field, FieldLabel } from "@/components/ui/field"
@@ -9,43 +11,56 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
-import { addDays, format, set } from "date-fns"
-import { CalendarIcon } from "lucide-react"
-import { type DateRange } from "react-day-picker"
 
-interface DateRangePickerProps {
-    value?: DateRange
-    onChange?: (range: DateRange | undefined) => void
+type DatePickerWithRangeProps = {
+  value?: DateRange
+  onChange?: (range: DateRange | undefined) => void
 }
 
-export function DateRangePicker({ value, onChange }: DateRangePickerProps) {
-const [internalDate, setInternalDate] = React.useState<DateRange | undefined>(
-    value
-)
+
+export function DatePickerWithRange({ value, onChange }: DatePickerWithRangeProps) {
+  const [internalDate, setInternalDate] = React.useState<DateRange | undefined>({
+  from: new Date(new Date().getFullYear(), 0, 20),
+  to: addDays(new Date(new Date().getFullYear(), 0, 20), 20),
+})
 
 const date = value ?? internalDate
 
-function handleSelect(range: DateRange | undefined) {
-    if(!value) {
-        setInternalDate(range)
-    }
-    onChange?.(range)
+const handleSelect = (newDate: DateRange | undefined) => {
+  if (onChange) {
+    onChange(newDate) // controlled
+  } else {
+    setInternalDate(newDate) // uncontrolled fallback
   }
-
+}
   return (
     <Field className="mx-auto w-60">
       <Popover>
         <PopoverTrigger asChild>
-          <Button variant="primary" id="date-picker-range" className="justify-start px-2.5 font-normal bg-white text-black border border-black hover:bg-gray-100">
+          <Button
+            variant="outline"
+            id="date-picker-range"
+            className="justify-start px-2.5 font-normal"
+          >
             <CalendarIcon />
-            {date?.from && date?.to
-            ? `${format(date.from, "LLL dd y")} - ${format(date.to, "LLL dd y")}`
-            : "Pick a date"}
+            {date?.from ? (
+              date.to ? (
+                <>
+                  {format(date.from, "LLL dd, y")} -{" "}
+                  {format(date.to, "LLL dd, y")}
+                </>
+              ) : (
+                format(date.from, "LLL dd, y")
+              )
+            ) : (
+              <span>Pick a date</span>
+            )}
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-auto p-0 bg-white text-black border border-gray-300 shadow-md" align="start">
+        <PopoverContent className="w-auto p-0" align="start">
           <Calendar
             mode="range"
+            defaultMonth={date?.from}
             selected={date}
             onSelect={handleSelect}
             numberOfMonths={2}
