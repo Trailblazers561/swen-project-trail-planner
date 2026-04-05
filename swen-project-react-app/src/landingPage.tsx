23a3ec4dea874117ca40aab77f6f2b17960a7d1b
Conlan <cac9809@rit.edu>
2026-04-05 18:49:14 -0400

    full screen map

--- swen-project-react-app/src/landingPage.tsx
+++ swen-project-react-app/src/landingPage.tsx
@@ -1,25 +1,38 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-
-//React leaflet for map?
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import Navbar from "./components/Navbar";
 
 const LandingPage = () => {
   const navigate = useNavigate();
 
   return (
 
-        <div>
+        <div className="h-screen w-screen relative">
 
-            <h1> Test Landing Page!!! </h1>
+                <div className="absolute top-6 right-6 z-[1000]">
+                    <Button
+                        variant="primary"
+                        onClick={() => navigate("/login")}
+                        >
+                        Login
+                    </Button>
+                </div>
 
-            <Button
-                variant="primary"
-                onClick={() => navigate("/login")}
-                >
-                Login
-            </Button>
-
-          </div>
+                <MapContainer
+                    center={[44.02, -73.82]} 
+                    zoom={9}
+                    scrollWheelZoom={true}
+                    className="h-full w-full"
+                    >
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            attribution="&copy; OpenStreetMap contributors"
+                        />
+                </MapContainer>
+                
+        </div>
   );
 };
 
