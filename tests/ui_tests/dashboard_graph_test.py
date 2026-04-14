ba42273764c4753a518de5a4730b553a5ec4d6ef
EzDubzEz <tlwtee3@gmail.com>
2026-04-14 03:36:29 -0400

    made add/edit trail dissapear wait dynamic

--- tests/ui_tests/dashboard_graph_test.py
+++ tests/ui_tests/dashboard_graph_test.py
@@ -111,7 +111,7 @@ def verify_granularity(driver, granularity: list[DashboardFilterDTO, list[Granul
 def verify_granularity_filter(driver, granularity_filter: DashboardFilterDTO, selected_trails: set[TrailDTO]):
     set_dashboard_filter_step = SetDashboardFiltersStep(driver, granularity_filter)
     set_dashboard_filter_step.run()
-    SH.wait(5)
+    SH.wait(10)
 
     retrieve_graph_step = RetrieveGraphStep(driver)
     retrieve_graph_step.run()
