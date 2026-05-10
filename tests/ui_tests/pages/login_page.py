a5f1ec54a554bdd2abfa104bbb6b668618eb98ee
EzDubzEz <tlwtee3@gmail.com>
2026-05-09 21:11:23 -0400

    fixed/updated login test but I commited all my changes

--- tests/ui_tests/pages/login_page.py
+++ tests/ui_tests/pages/login_page.py
@@ -10,27 +10,31 @@ class LoginPage:
     Login / Registration Page (/login)
     """
     root = (By.XPATH, "//div[@id='loginForm']")
-    email_input = (By.XPATH, "//input[@type='email']")
-    password_input = (By.XPATH, "//input[@type='password']")
-    sign_in_button = (By.XPATH, "//button[text()='Login']")
+    email_input = (By.XPATH, "//input[@data-testid='username-input']")
+    password_input = (By.XPATH, "//input[@data-testid='password-input']")
+    sign_in_button = (By.XPATH, "//button[@data-testid='login-button']")
+    sign_in_error = (By.XPATH, "//div[@data-testid='sign-in-error']//p")
 
     def __init__(self, driver: webdriver.Chrome):
         self.driver = driver
         SH.wait_for_element_appear(self.driver, self.root)
 
-    def sign_in(self, user: UserDTO, login_mode: LoginMode) -> str:
-        self._enter_email(user.email)
+    def sign_in(self, user: UserDTO, login_mode: LoginMode, username_login: bool) -> str:
+        if username_login:
+            self._enter_username(user.name)
+        else:
+            self._enter_username(user.email)
         self._enter_password(user.password)
         self._click_sign_in()
         if login_mode == LoginMode.NORMAL:
             SH.wait_for_element_disappear(self.driver, self.root, 30)
         elif login_mode == LoginMode.ALERT:
             SH.wait(.5)
-            return self._dismiss_alert()
+            return self._retrieve_alert()
         elif login_mode == LoginMode.VALIDATION:
             return self._retrieve_validation()
 
-    def _enter_email(self, email: str) -> None:
+    def _enter_username(self, email: str) -> None:
         SH.enter_text_to_element(self.driver, self.email_input, email)
 
     def _enter_password(self, password: str) -> None:
@@ -41,6 +45,9 @@ class LoginPage:
 
     def _dismiss_alert(self) -> str:
         return SH.dismiss_alert(self.driver)
+
+    def _retrieve_alert(self) -> str:
+        return SH.retrieve_text_from_element(self.driver, self.sign_in_error)
 
     def _retrieve_validation(self) -> str:
         return SH.retrieve_element_attribute(self.driver, self.email_input, "validationMessage") \
--- tests/ui_tests/steps/login/login_step.py
+++ tests/ui_tests/steps/login/login_step.py
@@ -8,12 +8,13 @@ class LoginStep:
     """
     Logs Into Application
     """
-    def __init__(self, driver: webdriver.Chrome, user: UserDTO, login_mode: LoginMode = LoginMode.NORMAL):
+    def __init__(self, driver: webdriver.Chrome, user: UserDTO, login_mode: LoginMode = LoginMode.NORMAL, username_login=True):
         self.driver = driver
         self.user = user
         self.login_mode = login_mode
+        self.username_login = username_login
         self.alert_validation_text: str
 
     def run(self):
         po = LoginPage(self.driver)
-        self.alert_validation_text = po.sign_in(self.user, self.login_mode)
+        self.alert_validation_text = po.sign_in(self.user, self.login_mode, self.username_login)
