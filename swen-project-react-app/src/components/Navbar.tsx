0bd0a7941e229354e6b0d234ac089fe876c990f4
Marrufof <fm8191@rit.edu>
2026-06-01 20:23:17 -0400

    added a navbar.css for styling

--- swen-project-react-app/src/components/Navbar.tsx
+++ swen-project-react-app/src/components/Navbar.tsx
@@ -2,31 +2,37 @@ import { UserIcon } from "@/components/ui/usericon"
 import logo from "@/assets/images/awa-logo.png"
 import { Role, useAuth } from "@/Context";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
     const { username, currentRole } = useAuth();
     return (
         <nav className="sticky top-0 w-full bg-white shadow-md z-50 mb-20px z-[1000]" id="navbar">
             <div className="top-0 left-0 w-lvw h-18 bg-[var(--color-navbar)] text-white flex items-center justify-between p-4">
-                <Link to="/home">
-                    <img src={logo} alt="Logo" className="h-full max-h-16 object-contain" />
-                </Link>
-                <div className="profile-info text-right justify-end flex items-center gap-2">
-                    {currentRole !== null ? (
-                        <div className="grid grid-rows-2 grid-cols-1">
-                            <div>
-                                <span data-testid="username">{username}</span>
+                    <Link to="/home">
+                        <img src={logo} alt="Logo" className="h-full max-h-16 object-contain" />
+                    </Link>
+                <div className="flex items-center gap-15 ml-auto">  
+                    <Link to="/home">Home</Link>
+                    <Link to="/dashboard">Dashboard</Link>
+                    {(currentRole === Role.Admin || currentRole === Role.Root) && <Link to="/privileges">Privileges</Link>}
+                    <div className="profile-info text-right justify-end flex items-center gap-2">
+                        {currentRole !== null ? (
+                            <div className="grid grid-rows-2 grid-cols-1">
+                                <div>
+                                    <span data-testid="username">{username}</span>
+                                </div>
+                                <div>
+                                    <div data-testid="user-role">{Role[currentRole]}</div>
+                                </div>
                             </div>
-                            <div>
-                                <div data-testid="user-role">{Role[currentRole]}</div>
+                        ) : (
+                            <div className="grid grid-rows-1">
+                                <div data-testid="user-role">Guest</div>
                             </div>
-                        </div>
-                    ) : (
-                        <div className="grid grid-rows-1">
-                            <div data-testid="user-role">Guest</div>
-                        </div>
-                    )}
-                    <UserIcon />
+                        )}
+                        <UserIcon />
+                    </div>
                 </div>
             </div>
         </nav>
