a821a5c4fb71783b4b0ef422233e4bb3713bc197
Marrufof <fm8191@rit.edu>
2026-06-14 21:07:55 -0400

    Added blank page for device management

---
+++ swen-project-react-app/src/deviceManagementPage.jsx
@@ -0,0 +1,13 @@
+
+const DeviceManagementPage = () => {
+
+    return (
+        <div>
+            <h1>Device Management Page</h1>
+            <p>This is where you can manage your devices.</p>
+        </div>
+    );
+}
+
+
+export default DeviceManagementPage;
