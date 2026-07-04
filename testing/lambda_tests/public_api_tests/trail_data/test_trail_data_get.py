2bd565de51e3807266e021646d49f89198edb4de
Richard Tang <rt6412@rit.edu>
2026-07-04 15:18:35 -0400

    trail_data_get lambda testing done (for now)

--- testing/lambda_tests/public_api_tests/trail_data/test_trail_data_get.py
+++ testing/lambda_tests/public_api_tests/trail_data/test_trail_data_get.py
@@ -1,6 +1,7 @@
 from testing.lambda_tests.test_data import DAY_LOG_DATA, DEVICE_TRAIL_DATA
 import importlib
 import json
+from datetime import datetime
 
 def load_module():
     """
@@ -18,15 +19,15 @@ def test_get_single_trail():
     module = load_module()
 
     event = {
-    "queryStringParameters": {
-        "start_time": "2026-01-01T00:00:00",
-        "end_time": "2026-01-02T00:00:00",
-        "granularity": "day",
-    },
-    "multiValueQueryStringParameters": {
-        "trail_id": ["1"],
-    },
-}
+        "queryStringParameters": {
+            "start_time": "2026-01-01T00:00:00",
+            "end_time": "2026-01-02T00:00:00",
+            "granularity": "day",
+        },
+        "multiValueQueryStringParameters": {
+            "trail_id": ["1"],
+        },
+    }
 
     #Act
     response = module.get_trail_data(event, None)
@@ -46,3 +47,165 @@ def test_get_single_trail():
 
     # Ensure all results belong to trail 1
     assert all(row["trail_id"] == 1 for row in body)
+
+def test_error_for_no_trail_id():
+    #Arrange
+    module = load_module()
+
+    event = {
+        "queryStringParameters": {
+            "start_time": "...",
+            "end_time": "...",
+            "granularity": "day",
+        },
+        "multiValueQueryStringParameters": {
+        },
+    }
+
+    #Act
+    response = module.get_trail_data(event, None)
+
+    #Assert
+    assert response["statusCode"] == 400
+
+def test_error_for_no_time_range():
+    #Arrange
+    module = load_module()
+
+    event = {
+        "queryStringParameters": {
+            "granularity": "day",
+        },
+        "multiValueQueryStringParameters": {
+            "trail_id": ["1"]
+        },
+    }
+
+    #Act
+    response = module.get_trail_data(event, None)
+
+    #Assert
+    assert response["statusCode"] == 400
+
+def test_error_for_invalid_trail_id():
+    #Arrange
+    module = load_module()
+
+    event = {
+        "queryStringParameters": {
+            "start_time": "2026-01-01T00:00:00",
+            "end_time": "2026-01-02T00:00:00",
+            "granularity": "day",
+        },
+        "multiValueQueryStringParameters": {
+            "trail_id": ["abc"]
+        },
+    }
+
+    #Act
+    response = module.get_trail_data(event, None)
+
+    #Assert
+    assert response["statusCode"] == 400
+
+def test_trail_filter():
+    #Arrange
+    module = load_module()
+
+    event = {
+        "queryStringParameters": {
+            "start_time": "2026-01-01T00:00:00",
+            "end_time": "2026-01-02T00:00:00",
+            "granularity": "day",
+        },
+        "multiValueQueryStringParameters": {
+            "trail_id": ["1"],
+        },
+    }
+
+    #Act
+    response = module.get_trail_data(event, None)
+
+    body = json.loads(response["body"])
+    #Assert
+    assert all(row["trail_id"] == 1 for row in body)
+
+def test_time_range_filter():
+    # Arrange
+    module = load_module()
+
+    start_time = "2026-01-01T00:00:00"
+    end_time = "2026-01-02T00:00:00"
+
+    event = {
+        "queryStringParameters": {
+            "start_time": start_time,
+            "end_time": end_time,
+            "granularity": "day",
+        },
+        "multiValueQueryStringParameters": {
+            "trail_id": ["1"],
+        },
+    }
+
+    # Act
+    response = module.get_trail_data(event, None)
+
+    # Assert
+    assert response["statusCode"] == 200
+
+    body = json.loads(response["body"])
+
+    assert len(body) == 2
+    assert body[0]["start"] < body[1]["start"]
+
+def test_empty_data():
+    #Arrange
+    module = load_module()
+
+    event = {
+        "queryStringParameters": {
+            "start_time": "1990-01-01T00:00:00",
+            "end_time": "1990-01-02T00:00:00",
+            "granularity": "day",
+        },
+        "multiValueQueryStringParameters": {
+            "trail_id": ["1"],
+        },
+    }
+
+    #Act
+    response = module.get_trail_data(event, None)
+
+    #Assert
+    assert response["statusCode"] == 200
+
+    body = json.loads(response["body"])
+
+    assert body == []
+
+def test_multi_trail():
+    #Arrange
+    module = load_module()
+
+    event = {
+        "queryStringParameters": {
+            "start_time": "2026-01-01T00:00:00",
+            "end_time": "2026-01-02T00:00:00",
+            "granularity": "day",
+        },
+        "multiValueQueryStringParameters": {
+            "trail_id": ["1", "2"],
+        },
+    }
+
+    #Act
+    response = module.get_trail_data(event, None)
+
+    #Assert
+    assert response["statusCode"] == 200
+
+    body = json.loads(response["body"])
+
+    assert len(body) > 0
+    assert all(row["trail_id"] in (1, 2) for row in body)
